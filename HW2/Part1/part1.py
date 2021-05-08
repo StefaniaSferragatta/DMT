3e6321848b7b676fa7df119c86e179298d416246
Stefania Sferragatta <sferragatta.stefania@gmail.com>
2021-05-08 13:16:03 +0200

    Add files via upload

---
+++ HW2/Part1/part1.py
@@ -0,0 +1,57 @@
+from surprise import Dataset,Reader 
+from surprise import NormalPredictor,BaselineOnly
+from surprise import KNNBasic,KNNWithMeans,KNNWithZScore,KNNBaseline
+from surprise import SVD,SVDpp,NMF,SlopeOne
+from surprise.model_selection import KFold, cross_validate
+import pandas as pd
+import numpy as np
+import time
+import os
+
+#load dataset1
+file_path = os.path.expanduser('./ratings_1.csv')
+print("Loading Dataset1...")
+reader = Reader(line_format='user item rating', sep=',', rating_scale=[1, 5], skip_lines=1)
+data1 = Dataset.load_from_file(file_path, reader=reader)
+print("Done.")
+
+#load dataset2
+file_path = os.path.expanduser('./ratings_2.csv')
+print("Loading Dataset2...")
+reader2 = Reader(line_format='user item rating', sep=',', rating_scale=[1, 10], skip_lines=1)
+data2 = Dataset.load_from_file(file_path, reader=reader2)
+print("Done.")
+
+def Recommendation_System(data):
+    # list of the algorithms and their name
+    algorithms= [NormalPredictor,BaselineOnly,KNNBasic,KNNWithMeans,KNNWithZScore,KNNBaseline,SVD,SVDpp,NMF,SlopeOne]
+    algo_names= ['NormalPredictor','BaselineOnly','KNNBasic','KNNWithMeans','KNNWithZScore','KNNBaseline','SVD','SVDpp','NMF','SlopeOne']
+    #useful data stracture
+    row=[]
+    table_df = pd.DataFrame()
+    #set the folds for the CV
+    kf = KFold(n_splits=5, random_state=0)
+    
+    #for each algorithm 
+    for current_algo in range(len(algorithms)):
+        #start = time.time()
+        result=cross_validate(algorithms[current_algo](), data, measures=['RMSE'], cv=kf, verbose=True,n_jobs=4)
+        print()
+        mean_rmse = '{:.4f}'.format(np.mean(result['test_rmse']))  
+        row.append([algo_names[current_algo], mean_rmse])
+        #end = time.time()-start #to see the excecution time
+    
+    #fill the dataframe    
+    table_df=table_df.append(row)
+    #set the cols name
+    table_df.columns =['Algorithm', 'RMSE']
+    #sort the dataframe in ascending order
+    table_df.sort_values(by=['RMSE'], inplace=True, ascending=False)
+    
+    #Show the execution time
+    #print('Execution time: ',round(end,2),' min')
+    
+    return table_df
+    
+Recommendation_System(data1)
+Recommendation_System(data2)
---
+++ HW2/Part1/part1_2.py
@@ -0,0 +1,135 @@
+from surprise import Dataset,Reader 
+from surprise import SVD,KNNBaseline
+from surprise.model_selection import KFold, cross_validate
+from surprise.model_selection import GridSearchCV,RandomizedSearchCV
+import numpy as np
+import pandas as pd
+import time
+import os
+
+'''Loading the datasets of the ratings'''
+
+#load dataset1
+file_path = os.path.expanduser('./ratings_1.csv')
+print("Loading Dataset1...")
+reader = Reader(line_format='user item rating', sep=',', rating_scale=[1, 5], skip_lines=1)
+data1 = Dataset.load_from_file(file_path, reader=reader)
+print("Done.")
+
+#load dataset2
+file_path = os.path.expanduser('./ratings_2.csv')
+print("Loading Dataset2...")
+reader2 = Reader(line_format='user item rating', sep=',', rating_scale=[1, 10], skip_lines=1)
+data2 = Dataset.load_from_file(file_path, reader=reader2)
+print("Done.")
+
+''' KNNBaseline hyperparameters tuning using RandomizedSearchCV() for both dataset1 and dataset2'''
+
+#function for the tuning of the hyperparameters using the RandomizedSearchCV()
+def tuning_KNNBaseline(data): 
+    param_grid = { 
+                "k":[5,10,20,25], #[20,40,50,60], 
+                "min_k":[1,5,7,9,13],#[1,3,5,8,11,13,15,18,21],
+                "sim_options":{
+                               'name': ["cosine","pearson_baseline"],  
+                               'user_based': [True, False],  
+                               'min_support': [1,5,10,12]#[3,5,8,11,13]
+                                },
+                "bsl_options":{
+                            'method': ['sgd'],
+                            'learning_rate':[0.001,0.007,0.1],#[0.002,0.005,0.01],
+                            'n_epochs':[20,30,40],#[50,100,150],
+                            'reg': [0.01,0.03,0.06]#[0.01,0.02,0.05]
+                            }
+                }
+    #to compute the execution time
+    start=time.time()
+    rcv = RandomizedSearchCV(KNNBaseline, param_grid, measures=['rmse'], cv=5,n_jobs=4) 
+    rcv.fit(data)
+    ex_time= round(time.time()-start,2)
+    #store the best score obtained
+    best_score = rcv.best_score['rmse']
+    #store the best parameters, usefull to find the avg_rmse
+    best_param = rcv.best_params['rmse']
+    return (ex_time,best_score,best_param)
+
+#tuning the hyperpameters for the KNNBaseline() on dataset1
+randcv_dt1 = tuning_KNNBaseline(data1)
+time1 = randcv_dt1[0]
+print("Execution time for dataset1: ", round(time1,2),'s')
+print()
+best_score1 = randcv_dt1[1]
+print("Best score for dataset1: ",best_score1)
+print()
+best_param1 = randcv_dt1[2]
+print(best_param1)
+
+#Use the best params to get the avg RMSE from KNNBaseline on dataset1
+kf = KFold(n_splits=5, random_state=0)
+current_algo=KNNBaseline(k=50, min_k=11, sim_options={'name': 'pearson_baseline', 'user_based':False,'min_support': 13}, bsl_options= {'method': 'sgd','learning_rate':0.005,'n_epochs': 50,'reg':0.05},verbose=True)
+cross_validate(current_algo, data1, measures=['RMSE'], cv=kf,n_jobs=4, verbose=True)
+
+#tuning the hyperpameters for the KNNBaseline() on dataset2
+randcv_dt2 = tuning_KNNBaseline(data2)
+time2 = randcv_dt2[0]
+print("Execution time for dataset2: ", round(time2,2), 's')
+print()
+best_score2 = randcv_dt2[1]
+print("Best score for dataset2: ",best_score2)
+print()
+best_param2 = randcv_dt2[2]
+print(best_param2)
+
+#Use the best params to get the avg RMSE from KNNBaseline on dataset2
+current_algo=KNNBaseline(k=25, min_k=9, sim_options={'name': 'pearson_baseline', 'user_based':True,'min_support':5}, bsl_options= {'method': 'sgd','learning_rate':0.001,'n_epochs': 30,'reg':0.01},verbose=True)
+cross_validate(current_algo, data2, measures=['RMSE'], cv=kf,n_jobs=4, verbose=True)
+
+
+''' SVD hyperparameters tuning using GridSearchCV() for both dataset1 and dataset2'''
+
+def tuning_SVD(data):    
+    param_grid = {"n_factors":[80,100,120,150],#[25,50,100,150],
+                  "lr_all": [0.005,0.008,0.1,0.5],#[0.005,0.01,0.5,1],
+                  "init_mean":[0.01,0.02,0.06,0.08],#[0.10,0.30,0.50,0.70],
+                  "reg_all":[0.06,0.1,0.4,0.5]#[0.01,0.05,0.07,0.1]
+                 }
+    #to compute the execution time
+    start=time.time()
+    gs = GridSearchCV(SVD, param_grid, measures=['rmse'], cv=5,n_jobs=4) 
+    gs.fit(data)
+    ex_time= round(time.time()-start,2)
+    #store the best score obtained
+    best_score = gs.best_score['rmse']
+    #store the best params configuration, usefull for computing the avg_rmse
+    best_param = gs.best_params['rmse']
+    return (ex_time,best_score,best_param)
+
+#tuning the hyperpameters for the SVD() on dataset1
+grid_cv1 = tuning_SVD(data1)
+time1 = grid_cv1[0]
+print("Execution time for dataset1: ", round(time1,2),'s')
+print()
+best_score1 = grid_cv1[1]
+print("Best score for dataset1: ",best_score1)
+print()
+best_param1 = grid_cv1[2]
+print(best_param1)
+
+#Use the best params to get the avg RMSE from SVD on dataset1
+current_algo= SVD(n_factors=100,lr_all=0.01,init_mean= 0.1,reg_all=0.07)
+cross_validate(current_algo, data1, measures=['RMSE'],cv=kf, n_jobs=4,verbose=True )
+
+#tuning the hyperpameters for the SVD() on dataset2
+grid_cv2= tuning_SVD(data2)
+time2 = grid_cv2[0]
+print("Execution time for dataset2: ", round(time2,2),'s')
+print()
+best_score2 = grid_cv2[1]
+print("Best score for dataset2: ",best_score2)
+print()
+best_param2 = grid_cv2[2]
+print(best_param2)
+
+#Use the best params to get the avg RMSE from SVD on dataset2
+current_algo= SVD(n_factors=150, lr_all=0.008,init_mean=0.02,reg_all=0.1)
+cross_validate(current_algo, data2, measures=['RMSE'], cv=kf, n_jobs=4, verbose=True)
