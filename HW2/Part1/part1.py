6537e416b366dee82628e8415c3486785e4eba65
Stefania Sferragatta <sferragatta.stefania@gmail.com>
2021-05-21 16:10:45 +0200

    Update part1.py

--- HW2/Part1/part1.py
+++ HW2/Part1/part1.py
@@ -2,6 +2,7 @@ from surprise import Dataset,Reader
 from surprise import NormalPredictor,BaselineOnly
 from surprise import KNNBasic,KNNWithMeans,KNNWithZScore,KNNBaseline
 from surprise import SVD,SVDpp,NMF,SlopeOne
+from surprise import CoClustering
 from surprise.model_selection import KFold, cross_validate
 import pandas as pd
 import numpy as np
@@ -24,34 +25,34 @@ print("Done.")
 
 def Recommendation_System(data):
     # list of the algorithms and their name
-    algorithms= [NormalPredictor,BaselineOnly,KNNBasic,KNNWithMeans,KNNWithZScore,KNNBaseline,SVD,SVDpp,NMF,SlopeOne]
-    algo_names= ['NormalPredictor','BaselineOnly','KNNBasic','KNNWithMeans','KNNWithZScore','KNNBaseline','SVD','SVDpp','NMF','SlopeOne']
+    algorithms= [NormalPredictor,BaselineOnly,KNNBasic,KNNWithMeans,KNNWithZScore,KNNBaseline,SVD,SVDpp,NMF,SlopeOne,CoClustering]
+    algo_names= ['NormalPredictor','BaselineOnly','KNNBasic','KNNWithMeans','KNNWithZScore','KNNBaseline','SVD','SVDpp','NMF','SlopeOne','CoClustering]
     #useful data stracture
     row=[]
     table_df = pd.DataFrame()
+    
     #set the folds for the CV
     kf = KFold(n_splits=5, random_state=0)
     
-    #for each algorithm 
+    #for each algorithm in the list
     for current_algo in range(len(algorithms)):
-        #start = time.time()
         result=cross_validate(algorithms[current_algo](), data, measures=['RMSE'], cv=kf, verbose=True,n_jobs=4)
         print()
+        #compute the mean
         mean_rmse = '{:.4f}'.format(np.mean(result['test_rmse']))  
+        #add the result to a list
         row.append([algo_names[current_algo], mean_rmse])
-        #end = time.time()-start #to see the excecution time
-    
-    #fill the dataframe    
+        
+    #fill the dataframe with the results obtained 
     table_df=table_df.append(row)
     #set the cols name
     table_df.columns =['Algorithm', 'RMSE']
-    #sort the dataframe in ascending order
-    table_df.sort_values(by=['RMSE'], inplace=True, ascending=False)
-    
-    #Show the execution time
-    #print('Execution time: ',round(end,2),' min')
-    
+    #sort the dataframe 
+    table_df.sort_values(by=['RMSE'], inplace=True, ascending=True)
+                 
     return table_df
     
+#invoke the function for the first dataset
 Recommendation_System(data1)
+#invoke the function for the second dataset
 Recommendation_System(data2)
